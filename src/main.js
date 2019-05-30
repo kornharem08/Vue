91225ea4ded66974a91956dc0b926b94c9777a58
tanakorn <tana_korn01@hotmail.com>
2019-05-30 15:37:34 +0700

    test

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import router from './router'
 Vue.config.productionTip = false
 
 new Vue({
+  el:'#app',
+  created() {
+    return (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'Messenger'));
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
