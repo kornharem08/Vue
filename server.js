f46ab2cc157d5fa787ee403836d2adc5fc50228d
tanakorn <tana_korn01@hotmail.com>
2019-05-30 15:00:22 +0700

    test

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'Messenger'));
+
