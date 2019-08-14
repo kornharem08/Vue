4e15fe5e321f1cf5ee023dc76e5482787eea5209
tanakorn <tana_korn01@hotmail.com>
2019-08-14 18:18:54 +0700

    'edit'

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-const history = require("connect-history-api-fallback");
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-app.use(
-    history({
-      disableDotRule: true,
-      verbose: true
-    })
-  );
-
-  app.get("/*", function(req, res) {
-    res.render(path.join(__dirname + "/dist/index.html"));
-  });
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
