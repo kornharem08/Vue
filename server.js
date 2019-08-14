674139fe9e0c0721ce14350fcbe07e88374f3e27
tanakorn <tana_korn01@hotmail.com>
2019-08-14 16:00:47 +0700

    'geturl'

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-const history = require('connect-history-api-fallback')
 app = express();
-const staticFileMiddleware = express.static(path.join(__dirname))
 app.use(serveStatic(__dirname + "/dist"));
 
-
-app.use(staticFileMiddleware)
-app.use(history())
-app.use(staticFileMiddleware)
-// app.use(history({
-//     disableDotRule: true,
-//     verbose: true
-//   }));
-  app.get('/', function (req, res) {
-    res.render(path.join(__dirname + '/dist/index.html'));
-  
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
