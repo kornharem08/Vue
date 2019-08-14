7e14464156f47432b631a346b7d275431634aa44
tanakorn <tana_korn01@hotmail.com>
2019-08-14 15:54:01 +0700

    'geturl'

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.use(history({
   }));
   app.get('/', function (req, res) {
     res.render(path.join(__dirname + '/dist/index.html'));
-  });
-  app.get('/about', function (req, res) {
-    res.render(path.join(__dirname + '/dist/index.html'));
-  });
+  
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
