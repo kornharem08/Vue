f4798c0359310ca79408be1989aed3b2df5a997a
tanakorn <tana_korn01@hotmail.com>
2019-08-15 14:12:53 +0700

    'Editlink'

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var path = require('path');
+var history = require('connect-history-api-fallback')
 var serveStatic = require('serve-static');
 app = express();
+
+
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
+
 app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 5000;
